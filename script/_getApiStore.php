f402cdd927fca639c593045e6215ebb7ead88e5f
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-07-14 19:27:06 +0300

    bugs fix

--- script/_getApiStore.php
+++ script/_getApiStore.php
@@ -31,5 +31,5 @@ function httpPost($url, $data)
     curl_close($curl);
     return $response;
 }
-echo (json_encode(array("msg"=>$msg,"response"=>($response)));
+echo (json_encode(array("msg"=>$msg,"response"=>$response)));
 ?>
