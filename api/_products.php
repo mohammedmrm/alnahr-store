66d55b890a6ada8d6150f274aadbf364d7ef9ef7
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-07-23 15:54:18 +0300

    bugs fix

--- api/_products.php
+++ api/_products.php
@@ -59,6 +59,33 @@ try{
   $data = getData($con,$query);
   $ps = getData($con,$count);
   $pages= ceil($ps[0]['count']/$limit);
+  $j=0;
+  foreach($data as $product){
+    $id = $product['id'];
+    $sql = "SELECT attribute.* FROM configurable_product
+            left join product on product.id = configurable_product.product_id
+            left join sub_option on sub_option.configurable_product_id = configurable_product.id
+            left join attribute on sub_option.attribute_id = attribute.id
+            where product.id =? group by attribute.id";
+    $res = getData($con,$sql,[$id]);
+    $i = 0;
+    foreach($res as $attr){
+      $sql = "select attribute_config.*, attribute.id as attribute_id from attribute
+              LEFT JOIN attribute_config on attribute_config.attribute_id = attribute.id
+              left join sub_option on sub_option.attribute_config_id = attribute_config.id
+              left join configurable_product on configurable_product.id = sub_option.configurable_product_id
+              where attribute.id = ? and configurable_product.product_id = ? GROUP by attribute_config.id";
+      $config = getData($con,$sql,[$attr['id'],$id]);
+      $res[$i]['config'] = $config;
+      $i++;
+    }
+
+    $data[$j]['attribute'] = $res;
+    $sql = "select * from images where product_id=?";
+    $images = getData($con,$sql,[$id]);
+    $data[$j]['images']= $images;
+    $j++;
+  }
   $success="1";
 } catch(PDOException $ex) {
    $data=["error"=>$ex];
