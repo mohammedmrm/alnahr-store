874af428c3dc4f2e6359f22c092f1c9937dcd5d0
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-07-28 16:57:31 +0300

    bugs fix

--- api/_products.php
+++ api/_products.php
@@ -2,8 +2,11 @@
 session_start();
 error_reporting(0);
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json");
-
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+ 
 require_once("_apiAccess.php");
 //access();
 require_once("../script/dbconnection.php");
