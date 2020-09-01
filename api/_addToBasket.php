9a50c9b049172a8fff4533579912ea6e70a07bef
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-09-01 20:40:06 +0300

    bugs

--- api/_addToBasket.php
+++ api/_addToBasket.php
@@ -5,6 +5,8 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 
+require_once("_apiAccess.php");
+access();
 require_once("../script/dbconnection.php");
 require_once("../script/_crpt.php");
 require_once("../config.php");
