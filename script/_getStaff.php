0636e8850176d28e92a5dbeab054179c1fa2ce03
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-09-07 17:26:05 +0300

    fix

--- script/_getStaff.php
+++ script/_getStaff.php
@@ -9,15 +9,15 @@ try{
   $query = "select staff.*,
             role.name as role_name
             from staff inner join role on role.id = staff.role_id
-            ";
+            where staff.company_id=?";
   }else{
    $query = "select staff.*,
             role.name as role_name
             from staff inner join branches on branches.id = staff.branch_id
             inner join role on role.id = staff.role_id
-            ";
+             where staff.company_id=?";
   }
-  $data = getData($con,$query);
+  $data = getData($con,$query,[$_SESSION['company_id']]);
   $success="1";
 } catch(PDOException $ex) {
    $data=["error"=>$ex];
