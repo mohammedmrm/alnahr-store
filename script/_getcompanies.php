af01a19f95f384405d681673f564df15916e3866
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-09-07 17:13:06 +0300

    bugs

--- script/_getcompanies.php
+++ script/_getcompanies.php
@@ -5,8 +5,8 @@ require("_access.php");
 access([1,2,3,4,5,6]);
 require("dbconnection.php");
 try{
-  $query = "select * from companies";
-  $data = getData($con,$query);
+  $query = "select * from companies where company_id=?";
+  $data = getData($con,$query,[$_SESSION['company_id']]);
   $success="1";
 } catch(PDOException $ex) {
    $data=["error"=>$ex];
