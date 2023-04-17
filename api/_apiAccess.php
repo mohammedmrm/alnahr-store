4d84d66ddd21f02e60ca970f96852b6a953f4a49
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2023-04-17 19:05:11 +0200

    bug

--- api/_apiAccess.php
+++ api/_apiAccess.php
@@ -1,31 +1,34 @@
 <?php
-if(!isset($_SESSION)){
- session_start();
+if (!isset($_SESSION)) {
+   session_start();
 }
+
+header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 $username = $_REQUEST['username'];
 $password = $_REQUEST['password'];
 require_once("../script/dbconnection.php");
-if(!empty($password) && !empty($username)){
-  $sql = 'select * from staff where phone=?';
-  $loginres  = getData($con,$sql,[$username]);
-  if(count($loginres) == 1 && password_verify($password,$loginres[0]['password'])){
-     $msg = 1;
-     $code = 200;
-     $userid = $loginres[0]['id'];
-     $head_company_id= $loginres[0]['company_id'];
-     $userrole= $loginres[0]['role_id'];
-  }else{
-     $msg ="incorrect username or password";
-     $code = 300;
-  }
-}else{
-     $msg ="username and password required";
-     $code = 301;
+if (!empty($password) && !empty($username)) {
+   $sql = 'select * from staff where phone=?';
+   $loginres  = getData($con, $sql, [$username]);
+   if (count($loginres) == 1 && password_verify($password, $loginres[0]['password'])) {
+      $msg = 1;
+      $code = 200;
+      $userid = $loginres[0]['id'];
+      $head_company_id = $loginres[0]['company_id'];
+      $userrole = $loginres[0]['role_id'];
+   } else {
+      $msg = "incorrect username or password";
+      $code = 300;
+   }
+} else {
+   $msg = "username and password required";
+   $code = 301;
 }
 
-function access(){
-  if($GLOBALS['msg']!=1){
-     die(json_encode(['message'=>$GLOBALS['msg'],'code'=>$GLOBALS['code']]));
-  }
+function access()
+{
+   if ($GLOBALS['msg'] != 1) {
+      die(json_encode(['message' => $GLOBALS['msg'], 'code' => $GLOBALS['code']]));
+   }
 }
